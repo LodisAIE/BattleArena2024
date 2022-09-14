9abc4a657d977b94a7f6177a69e896f8c0168ce4
Lodis <lodis.perkins@aie.edu>
2022-09-14 17:06:45 -0500

    Added array exercise

--- Game.cs
+++ Game.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HelloWorld
 {
-    struct Monster
+    struct Character
     {
         public string Name;
         public float Health;
@@ -13,10 +13,45 @@ namespace HelloWorld
         public float Armor;
         public float Defense;
         public bool IsAlive;
+        public Attack[] Attacks;
+    }
+
+    struct Attack
+    {
+        public string Name;
+        public float Damage;
+        public int CastTime;
+        public int Cooldown;
     }
 
     class Game
     {
+
+        //Initialize monsters
+        Character terry;
+        Character coldSteel;
+        Character nemisis;
+        Character kidKnuckles;
+        Character sumtinGuuud;
+        Character currentFighter1;
+        Character currentFighter2;
+
+        int currentScene =  0;
+        bool gameOver = false;
+        Character[] characters;
+
+        void PrintAllStats()
+        {
+            for (int i = 0; i < characters.Length; i++)
+            {
+                Console.WriteLine(characters[i].Name);
+                Console.WriteLine(characters[i].Health);
+                Console.WriteLine(characters[i].Attack);
+                Console.WriteLine(characters[i].Strength);
+                Console.WriteLine(characters[i].Defense);
+            }
+        }
+
         //Make a function that can calculate and return the total damage a monster would recieve.
         //Total damage is going to be opponents base damage subtracted by the defending monsters defense.
 
@@ -33,7 +68,7 @@ namespace HelloWorld
         }
 
         //Function overloading example vv
-        float CalculateDamage(Monster monster1, Monster monster2)
+        float CalculateDamage(Character monster1, Character monster2)
         {
             if (monster1.Attack <= monster2.Defense)
             {
@@ -45,7 +80,7 @@ namespace HelloWorld
             return totalDamage;
         }
 
-        void PrintStats(Monster monster)
+        void PrintStats(Character monster)
         {
             Console.WriteLine(monster.Name + "'s Stats:");
             Console.WriteLine("Health: " + monster.Health);
@@ -55,9 +90,13 @@ namespace HelloWorld
             Console.WriteLine("Armor: " + monster.Armor);
         }
 
-        float Attack(Monster attacker, Monster defender)
+        float Attack(ref Character attacker, int attackIndex, ref Character defender)
         {
-            float damageTaken = CalculateDamage(attacker, defender);
+            Attack currentAttack = attacker.Attacks[attackIndex];
+
+            float damage = currentAttack.Damage;
+
+            float damageTaken = CalculateDamage(damage, defender.Defense);
             defender.Health -= damageTaken;
 
             if (defender.Health < 0)
@@ -68,27 +107,141 @@ namespace HelloWorld
             return damageTaken;
         }
 
-        
-        void ChangeNumber(int number)
+        void ScaleStats(ref Character monster, float scale)
         {
-            number = 2;
+            monster.Attack *= scale;
+            monster.Defense *= scale;
+            monster.Health *= scale;
+            monster.Armor *= scale;
+            monster.Strength *= scale;
+        }
+
+        void DisplayStartMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Super Mecha Death Battle: Volume 8 Reloaded V5.0 Z");
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            currentScene = 1;
+        }
+
+        void DisplayFightScene()
+        {
+            Console.Clear();
+
+            //Display current stats
+            PrintStats(currentFighter1);
+            Console.WriteLine();
+            PrintStats(currentFighter2);
+
+            //Make the first fighter punch the second
+            float damageTaken = Attack(ref currentFighter1, 2, ref currentFighter2);
+
+            //Display damage message for player feedback
+            Console.WriteLine(currentFighter2.Name + " took " + damageTaken + " damage!");
+            Console.ReadKey();
+
+            damageTaken = Attack(ref currentFighter2, 0, ref currentFighter1);
+
+            //Display damage message for player feedback
+            Console.WriteLine(currentFighter1.Name + " took " + damageTaken + " damage!");
+            Console.ReadKey();
+            
+            if (currentFighter1.Health <= 0 || currentFighter2.Health <= 0)
+                currentScene = 2;
+        }
+
+        void DisplayBattleResult()
+        {
+            //Create default result message
+            string resultMessage = "The winner is ";
+
+            if (currentFighter1.Health > 0)
+            {
+                resultMessage += currentFighter1.Name;
+            }
+            else if (currentFighter2.Health > 0)
+            {
+                resultMessage += currentFighter2.Name;
+            }
+            else
+            {
+                resultMessage = "Match is draw";
+                currentFighter1 = characters[2];
+                currentFighter2 = characters[3];
+            }
+
+            Console.WriteLine(resultMessage);
+            Console.ReadKey();
+            Console.Clear();
+
+            gameOver = true;
+        }
+
+        /// <summary>
+        /// Called when the game begins
+        /// </summary>
+        void Start()
+        {
+            //Initialize attacks
+            Attack swordSlash = new Attack { Name = "Sword Slash", Damage = 12 };
+            Attack punch = new Attack { Name = "Punch", Damage = 15 };
+            Attack kick = new Attack { Name = "Kick", Damage = 20 };
+            Attack kamehameha = new Attack { Name = "Kamehameha", Damage = 8999, CastTime = 5 };
+
+            //Initialize fighters
+            terry = new Character { Name = "Terry", Attack = 8f, Strength = 0, Defense = 0, Armor = 0, Health = 100f, IsAlive = true };
+            coldSteel = new Character { Name = "ColdSteel", Attack = 8f, Strength = 0, Defense = 0, Armor = 0, Health = 100f, IsAlive = true };
+            nemisis = new Character { Name = "Nemesis", Attack = 10, Strength = 2, Defense = 100000, Armor = 0, Health = 720000, IsAlive = true };
+            kidKnuckles = new Character { Name = "KidKnickles", Attack = 10, Strength = 30010, Defense = 10, Health = 10, Armor = 10, IsAlive = true };
+            sumtinGuuud = new Character { Name = "SumtinGUUUD", Attack = 5, Strength = 525000, Defense = 600, Armor = 1738, Health = 4000, IsAlive = true };
+
+            terry.Attacks = new Attack[] { swordSlash, kamehameha, punch };
+            coldSteel.Attacks = new Attack[] { punch, kick, kamehameha };
+
+            characters = new Character[] { terry, coldSteel, nemisis, kidKnuckles, sumtinGuuud };
+
+            //Set current fighters
+            currentFighter1 = terry;
+            currentFighter2 = coldSteel;
+        }
+
+        /// <summary>
+        /// Called every time the game loops
+        /// </summary>
+        void Update()
+        {
+            if (currentScene == 0)
+            {
+                DisplayStartMenu();
+            }
+            else if (currentScene == 1)
+            {
+                DisplayFightScene();
+            }
+            else if (currentScene == 2)
+            {
+                DisplayBattleResult();
+            }
+        }
+
+        void End()
+        {
+            Console.WriteLine("Thank a you so much for playin mah game");
         }
 
         public void Run()
         {
-            //Initialize monsters
-            Monster monster1 = new Monster { Name = "Terry", Attack = 8999f, Strength = 1.5f, Defense = 6f, Armor = 2.3f, Health = 20f, IsAlive = true};
-            Monster monster2 = new Monster { Name = "ColdSteel", Attack = 8999f, Strength = 2.1f, Defense = 2043f, Armor = 1.3f, Health = 2.3f, IsAlive = true };
-            int number1 = 5;
+            Start();
 
-            //number = number1
-            ChangeNumber(number1);
+            while (!gameOver)
+            {
+                Update();
+            }
 
-            //Monster 1 attacks monster 2
-            float damageTaken = Attack(monster1, monster2);
-
-            Console.WriteLine("Monster 2 has taken " + damageTaken + " damage!");
-            PrintStats(monster2);
+            End();
         }
     }
 }
